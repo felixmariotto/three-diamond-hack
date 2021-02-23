c343f4573bec664fe63cf38835779ccb55bc83c9
Felix Mariotto <felixmariotto@gmail.com>
2021-02-23 14:50:23 +0100

    make fragment gemFrontShader use fragment from the inner part of the backfaces

--- src/gemFrontShaders.js
+++ src/gemFrontShaders.js
@@ -6,22 +6,32 @@ vertex: `
 
 	varying vec2 vUv;
 	varying vec2 vCoords;
+	varying vec2 cCoords;
 
 	void main() {
+
 		vUv = uv;
-		vec3 scaleDir = ( position - islandCenter ) * -1.0;
-		vec3 pos = position + ( scaleDir * 0.25 );
-		// vec3 pos = position;
+		// vec3 scaleDir = ( position - islandCenter ) * -1.0;
+		// vec3 pos = position + ( scaleDir * 0.25 );
+		vec3 pos = position;
+
 		vec4 v = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 );
 		vCoords.x = ( v.x / v.z ) * 0.5 + 0.5;
 		vCoords.y = ( v.y / v.z ) * 0.5 + 0.5;
+
+		vec4 c = projectionMatrix * modelViewMatrix * vec4( islandCenter, 1.0 );
+		cCoords.x = ( c.x / c.z ) * 0.5 + 0.5;
+		cCoords.y = ( c.y / c.z ) * 0.5 + 0.5;
+
 		gl_Position = v;
+
 	}
 `,
 
 fragment: `
 	varying vec2 vUv;
 	varying vec2 vCoords;
+	varying vec2 cCoords;
 
 	uniform sampler2D u_shiftRT;
 	uniform sampler2D u_gemsBackRT;
@@ -32,7 +42,7 @@ fragment: `
 		vec2 shift = vec2( shiftData.x * 2.0 - 1.0, shiftData.y * 2.0 - 1.0 );
 		shift *= shiftData.z * -0.2;
 
-		vec2 uv = vCoords + shift;
+		vec2 uv = ( ( vCoords + cCoords ) / 2.0 ) + shift;
 
 		vec4 texel = texture2D( u_gemsBackRT, uv );
 
